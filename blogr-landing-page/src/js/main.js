7c263ed05c9d32623d1d338070d9524098b5101f
Raj Cutinha <rajath.cutinha@gmail.com>
2021-03-21 16:47:21 +0100

    project blogr v1

--- blogr-landing-page/src/js/main.js
+++ blogr-landing-page/src/js/main.js
@@ -1,3 +1,28 @@
 const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav ul");
+const parentLinks = document.querySelectorAll("nav > ul > li > a");
+const subLinks = document.querySelectorAll("ul li ul li a");
 
-console.log(burger);
+burger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+parentLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    parentLinks.forEach((link) =>
+      link.parentElement.classList.remove("active")
+    );
+    this.parentElement.classList.add("active");
+  });
+});
+
+subLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+    parentLinks.forEach((link) =>
+      link.parentElement.classList.remove("active")
+    );
+  });
+});
