44722ddeafa2262452eb5c818feaa28a5e2c0ab5
RajCutinha <rajath.cutinha@fit.fichtner.de>
2022-07-29 10:52:03 +0200

    added conditional render on login

--- entertainment-web-app/src/app/login/login.component.ts
+++ entertainment-web-app/src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  url!: string | undefined;
+  signUp!: boolean;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.url.subscribe((url) => (this.url = url[0]?.path));
+    this.signUp = this.loginHandler(this.url);
   }
 
+  loginHandler(currentUrl: string | undefined): boolean {
+    if (currentUrl === 'signup') {
+      return true;
+    }
+
+    return false;
+  }
 }
