2ab2e0395c09144de123522592c805b97f892c99
RajCutinha <rajath.cutinha@fit.fichtner.de>
2022-08-10 17:30:12 +0200

    added bcrypt

--- entertainment-web-app/backend/src/main.ts
+++ entertainment-web-app/backend/src/main.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
-import { ClientConfig } from "pg";
+import { ClientConfig, Pool } from "pg";
 import express from "express";
+import bcrypt from "bcrypt";
+
+interface Registration {
+  email: string;
+  password: string;
+}
 
 const config: ClientConfig = {
   port: +process.env.PGPORT!,
@@ -10,7 +16,33 @@ const config: ClientConfig = {
   database: process.env.PGDATABASE,
 };
 
+const postGres = new Pool(config);
+postGres.connect().then((res) => res);
+postGres.query("Select * From users", (err, res) => {
+  console.log(res.rows);
+});
+
 const server = express();
 const port: number = Number(process.env.PORT) || 3000;
+
+server.use(express.json());
+server.use(express.text());
+
+server.get("/api/user", (req, res) => {});
+
+server.post("/api/user", async (req, res) => {
+  const { email, password }: Registration = req.body;
+  const hash = await bcrypt.hash(password, 10);
+
+  postGres.query("Insert Into users(email, password) Values($1, $2)", [
+    email,
+    hash,
+  ]);
+  postGres.query("Select * From users", (err, res) => {
+    console.log(res.rows);
+  });
+
+  res.status(200).send();
+});
 
 server.listen(port, () => console.log("Server is running on Port " + port));
